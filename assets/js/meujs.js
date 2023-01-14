e6571f0c6b56c19163cbb890dd175539215bd8a5
ornar3 <ornar29@gmail.com>
2023-01-14 15:57:32 +0000

    pap resto

---
+++ assets/js/meujs.js
@@ -0,0 +1,42 @@
+var ctx = document.getElementById("leite").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Domingo", "aaa", "34iuhf3gf", "Wednesday", "Thursday", "Friday", "Saturday"],
+    datasets: [{
+      label: 'Statistics',
+      data: [460, 458, 330, 502, 430, 610, 488],
+      borderWidth: 2,
+      backgroundColor: '#6777ef',
+      borderColor: '#6777ef',
+      borderWidth: 2.5,
+      pointBackgroundColor: '#ffffff',
+      pointRadius: 4
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        gridLines: {
+          drawBorder: false,
+          color: '#f2f2f2',
+        },
+        ticks: {
+          beginAtZero: true,
+          stepSize: 150
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          display: false
+        }
+      }]
+    },
+  }
+});
