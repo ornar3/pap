cb57787583001b7413159626f79f9bc28d59ed4f
ornar3 <ornar29@gmail.com>
2023-01-14 16:34:33 +0000

    Update DBConnection.php

--- DBConnection.php
+++ DBConnection.php
@@ -1,7 +1,7 @@
 	
 	<?php
 	$db_name= 'pap';
-	$link= mysqli_connect('localhost','root','',$db_name);
+	$link= mysqli_connect('localhost','root','',$db_name,3000);
 	if(!$link){
 		die('Could not connect: '.mysql_error());
 	}
